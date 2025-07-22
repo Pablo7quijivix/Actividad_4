6112d1e360534803772cb39eb829027642868323
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-21 20:02:12 -0600

    Commit conditional name (aplicando el ingles jaja no chatgpt :) )

--- tarea_actividad_4.py
+++ tarea_actividad_4.py
@@ -2,7 +2,12 @@
 codigo numero 1
 simulador de votacion con validacion cruzada
 '''
-name=input("Ingrese su nombre completo: ")
-dpi=int(input("ingrese su numero de DPI: "))
-department= input("ingrese su departamento de origen: ")
-yearofbirth=int(input("Ingrese su fecha de nacimiento: "))
+while True:
+    name=input("Ingrese su nombre (nombre y apellido): ") #ejemplo de un nombre completo
+    #Arwen Dayanne Rivera Rios, este nombre contiene aprox 25 caracteres, contando espacios
+    dpi=input("ingrese su numero de DPI (debe contener 13 digitos): ")
+    department= input("ingrese su departamento de origen: ")
+    yearofbirth=int(input("Ingrese su fecha de nacimiento: "))
+
+    if len(name) < 5:
+        print ("Nombre incorrecto,  ingrese un nombre mayor a 5 caracteres")
