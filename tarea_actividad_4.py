57aacaa4f9faca1425807fc637668ee34df02154
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-21 19:49:03 -0600

    Commit ingreso de datos

--- tarea_actividad_4.py
+++ tarea_actividad_4.py
@@ -1,2 +1,8 @@
-#codigo numero 1
-print("hola")
+'''
+codigo numero 1
+simulador de votacion con validacion cruzada
+'''
+name=input("Ingrese su nombre completo: ")
+dpi=int(input("ingrese su numero de DPI: "))
+department= input("ingrese su departamento de origen: ")
+yearofbirth=int(input("Ingrese su fecha de nacimiento: "))
