b483e89e07acc22faab56cc763e5ebbc225d0ef0
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-27 13:29:26 -0600

    1 commit oicial

--- Actividad_4_actualizado.py
+++ Actividad_4_actualizado.py
@@ -1,2 +1,13 @@
 #actividad numero 1
-print ("hola")
+name= input("Ingerese su nombre completo: ")
+dpi=input("Ingrese su DPI: ")
+deparment= input("Ingrese departamento: ")
+yearofbirth= int(input("Ingrese año de nacimiento: "))
+
+mayor_edad= 2025-yearofbirth
+dpi2= len(dpi) # numero de caracteres
+name2= len(name)
+
+if name2 > 5
+
+
