54277c52ce1fa4b2ef4fbd4d3e96b48af6af3208
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-27 17:48:58 -0600

    4 commit oficial

--- Actividad_4_actualizado.py
+++ Actividad_4_actualizado.py
@@ -9,11 +9,25 @@ dpi2= len(dpi) # numero de caracteres
 name2= len(name)
 
 if name2 > 5:
-    print("Nombre valido")
-    if dpi2 != 13:
-        print("DPI Incorrecto")
+    #print("Nombre valido")
+    if dpi2 == 13:
+        #print("DPI Correcto")
         if deparment.lower() == "peten" or deparment.lower() == "alta verapaz":
-            print("Los departamentos de Petén y Alta Verapaz votan desde los 17 años")
+           # print("Los departamentos de Petén y Alta Verapaz votan desde los 17 años")
+            if mayor_edad >= 18:
+                print("__________Votante Válido_________")
+                print(f"Bienvenido {name} su centro de votación está es {deparment} ")
+            else:
+                print(f"======USUARIO NO PUEDE VOTAR POR: =====")
+                print("NO es mayor de edad")
+        else:
+            print("__________Votante Válido_________")
+            print(f"Bienvenido {name} su centro de votación está es {deparment} ")
+    else:
+        print("DENEGADO POR DPI InValido")
+else:
+    print(f"======USUARIO NO PUEDE VOTAR POR: =====")
+    print("NO es mayor de edad")
 
 
 
