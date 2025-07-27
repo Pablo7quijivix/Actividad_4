232fee66727575bce95ac2500b0a714dd930db4f
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-27 17:14:02 -0600

    3 commit oficial

--- Actividad_4_actualizado.py
+++ Actividad_4_actualizado.py
@@ -8,6 +8,13 @@ mayor_edad= 2025-yearofbirth
 dpi2= len(dpi) # numero de caracteres
 name2= len(name)
 
-if name2 > 5
+if name2 > 5:
+    print("Nombre valido")
+    if dpi2 != 13:
+        print("DPI Incorrecto")
+        if deparment.lower() == "peten" or deparment.lower() == "alta verapaz":
+            print("Los departamentos de Petén y Alta Verapaz votan desde los 17 años")
+
+
 
 
