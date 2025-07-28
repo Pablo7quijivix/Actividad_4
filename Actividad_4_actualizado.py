b5c95be9f9740291903c13900e101403c5ccbe1e
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-27 21:52:14 -0600

    1 commit del 2do codigo

--- Actividad_4_actualizado.py
+++ Actividad_4_actualizado.py
@@ -1,4 +1,5 @@
 #actividad numero 1
+'''
 name= input("Ingerese su nombre completo: ")
 dpi=input("Ingrese su DPI: ")
 deparment= input("Ingrese departamento: ")
@@ -29,6 +30,28 @@ if name2 > 5:
 else:
     print(f"======USUARIO NO PUEDE VOTAR POR: =====")
     print("---NO es mayor de edad---")
+'''
+
+
+#CODIGO 2 CALCULADORA DE IMPUESTOS PROGRESIVOS + DEDUCCIONES
+ingreso_anual= int(input("Digite su ingreso anual: "))
+no_dependientes= int(input("Digite su numero de dependientes: "))
+no_dependientes2 = no_dependientes * 1000
+
+if 0 <= ingreso_anual <= 30000:
+    descuento1= ingreso_anual * 0.05 #descuento1 del 5%
+    if 30001 <= ingreso_anual >= 60000:
+        descuento2 = ingreso_anual *0.1 #descuento2 del 10%
+        if 60001 <= ingreso_anual >=100000:
+            descuento3 = ingreso_anual * 0.15 #descuento3 del 15%
+            if ingreso_anual > 100000:
+                descuento4 = ingreso_anual * 0.2 #descuento4 del 20%
+                if ingreso_anual < 40000 and no_dependientes > 2:
+                    print(f"NO PAGA IMPUESTOS")
+                else:
+                    pass
+            else:
+
 
 
 
