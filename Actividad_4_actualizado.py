c474a674c07e7a21ea691ce285d6978d2185608e
Pablo Quijivix <davidquijivixsuchi@gmail.com>
2025-07-27 23:52:54 -0600

    2 commit del 2do codigo

--- Actividad_4_actualizado.py
+++ Actividad_4_actualizado.py
@@ -31,28 +31,29 @@ else:
     print(f"======USUARIO NO PUEDE VOTAR POR: =====")
     print("---NO es mayor de edad---")
 '''
-
+from pickletools import read_string1, read_uint2
 
 #CODIGO 2 CALCULADORA DE IMPUESTOS PROGRESIVOS + DEDUCCIONES
-ingreso_anual= int(input("Digite su ingreso anual: "))
-no_dependientes= int(input("Digite su numero de dependientes: "))
+ingreso_anual= float(input("Digite su ingreso anual: "))
+no_dependientes= float(input("Digite su numero de dependientes: "))
+
 no_dependientes2 = no_dependientes * 1000
 
+des1 = 0
+des2 = 0
+des3 = 0
+des4 = 0
 if 0 <= ingreso_anual <= 30000:
-    descuento1= ingreso_anual * 0.05 #descuento1 del 5%
-    if 30001 <= ingreso_anual >= 60000:
-        descuento2 = ingreso_anual *0.1 #descuento2 del 10%
+    des1= float(ingreso_anual * 0.05)
+    if 30001 <= ingreso_anual <= 60000:
+        des2 =float (ingreso_anual *0.1)
         if 60001 <= ingreso_anual >=100000:
-            descuento3 = ingreso_anual * 0.15 #descuento3 del 15%
+            des3 = float(ingreso_anual * 0.15)
             if ingreso_anual > 100000:
-                descuento4 = ingreso_anual * 0.2 #descuento4 del 20%
+                des4 = float (ingreso_anual * 0.2)
                 if ingreso_anual < 40000 and no_dependientes > 2:
-                    print(f"NO PAGA IMPUESTOS")
-                else:
-                    pass
-            else:
-
-
+                    print("=======CALCULADORA DE IMPUESTOS PROGRESIVOS + DEDUCCIONES=======")
+                    p
 
 
 
